52a4c123a44f59994f392ff67d66b996eddc92e4
Luc_cute <lucgiang0904@gmail.com>
2025-09-06 13:31:05 +0700

    upadte project

---
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -0,0 +1,46 @@
+package com.javaweb.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.model.BuildingDTO;
+import com.javaweb.service.BuildingService;
+
+
+@RestController
+public class BuildingAPI {
+	
+	@Autowired
+	private BuildingService buildingService;
+	
+	@GetMapping(value ="/api/building/")
+	public List<BuildingDTO> getBuilding(@RequestParam(name = "name",required = false) String name,
+										@RequestParam(name = "districtid", required = false) Long districtId,
+										@RequestParam(name = "typeCode", required = false) List<String> typeCode) {
+										
+		
+		List<BuildingDTO> result = buildingService.findAll(name,districtId);
+		return result; 
+		
+	}
+//	public void valiDate(BuildingDTO buildingDTO) {
+//		if(buildingDTO.getName() == null || buildingDTO.getName().equals("") || buildingDTO.getNumberOfBasement() == null) {
+//			throw new FieldRequiredException("name or numberofbasement is null");
+//		}
+//	}
+//	@PostMapping(value = "/api/building/")
+//	public void getBuilding2(@RequestBody BuildingDTO buildingDTO) {
+//		System.out.println("ok");
+//	}
+//	
+	@DeleteMapping(value ="/api/building/{id}")
+	public void deleteBuilding(@PathVariable Integer id) {
+		System.out.println("Da xoa toa nha co id la" + id + " roi nhe!");
+	}
+}
---
+++ src/main/java/com/javaweb/controllerAdvice/ControllerAdvisor.java
@@ -0,0 +1,46 @@
+package com.javaweb.controllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.valves.ErrorReportValve;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.javaweb.model.ErrorResponseDTO;
+
+import customexception.FieldRequiredException;
+
+
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<Object> handlerArithmeticException(
+            ArithmeticException ex, WebRequest request) {
+
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setError(ex.getMessage());
+        List<String> details = new ArrayList<>();
+        details.add("Số nguyên làm sao chia được cho không!");
+        errorResponseDTO.setDetail(details);
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+	
+	
+	@ExceptionHandler(FieldRequiredException.class)
+    public ResponseEntity<Object> handlerFieldRequiredException(
+    		FieldRequiredException ex, WebRequest request) {
+
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setError(ex.getMessage());
+        List<String> details = new ArrayList<>();
+        details.add("Check lại name hoặc numberofbasement đi bởi vì đang bị null đó");
+        errorResponseDTO.setDetail(details);
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_GATEWAY);
+    }
+}
