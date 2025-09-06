5f0446794215c5c94cb7dd5c14b0ca7a3b7afa74
Luc_cute <lucgiang0904@gmail.com>
2025-09-06 15:01:50 +0700

    Update

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -22,7 +22,7 @@ public class BuildingAPI {
 	@GetMapping(value ="/api/building/")
 	public List<BuildingDTO> getBuilding(@RequestParam(name = "name",required = false) String name,
 										@RequestParam(name = "districtid", required = false) Long districtId,
-										@RequestParam(name = "typeCode", required = false) List<String> typeCsode) {
+										@RequestParam(name = "typeCode", required = false) List<String> typeCode) {
 										
 		
 		List<BuildingDTO> result = buildingService.findAll(name,districtId);
